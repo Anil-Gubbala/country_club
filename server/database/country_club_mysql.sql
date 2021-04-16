5a3f8cb45545579a87e475a6651528f7fad96126
Esha Sah <65586427+eshasah@users.noreply.github.com>
2021-04-16 16:16:36 -0700

    1.Same data type for all ids, 2. event_type table is not in our erd and is not even referenced in any other table, so removed it. 3. Few tables were missing relationship according to erd, so added them.

--- server/database/country_club_mysql.sql
+++ server/database/country_club_mysql.sql
@@ -2,7 +2,7 @@ drop database  if exists countryclub;
 create database countryclub;
 use countryclub;
 
-CREATE TABLE user (
+create table user (
   user_id varchar(50) NOT NULL,
   f_name varchar(100) NOT NULL,
   l_name varchar(100) NOT NULL,
@@ -17,134 +17,123 @@ CREATE TABLE user (
 );
 
 
-Create table member
+create table member
 (
 	 user_id varchar(50) unique not null,
 	 membership_type varchar(50) not null,
-	 start_date date,
-	 end_date date,
+	 start_date date not null,
+	 end_date date not null,
 	 foreign key (user_id) references user (user_id) on update cascade on delete cascade 
 );
 
-Create table dependent
+create table dependent
 (
-	name varchar(50) not null,
+	name varchar(100) not null,
 	user_id varchar(50) not null,
 	relationship varchar(50),
-	primary key (name, user_id),
+	primary key (user_id, name),
 	foreign key (user_id) references user (user_id) on update cascade on delete cascade
 );
 
-Create table venue
+create table venue
 (
-	venue_id int not null,
-	venue_name varchar(25),
+	venue_id varchar(50) not null,
+	venue_name varchar(50),
 	venue_type varchar(25),
 	primary key (venue_id)    
 );
 
-Create table sports
+create table sports
 (
 	sport_id varchar(50) not null,
 	s_name varchar(50),
-	venue_id int,
+	venue_id varchar(50),
 	primary key (sport_id),
 	foreign key(venue_id) references venue(venue_id) on update cascade on delete cascade
 );
 
-Create table time_slot
+create table time_slot
 (
-	ts_id varchar(15) not null,
+	ts_id varchar(50) not null,
 	slot_start_time time,
 	slot_end_time time,
-	sport_id varchar(50),
-	primary key(ts_id),
-	foreign key (sport_id) references sports(sport_id) on delete cascade on update cascade
+	primary key(ts_id)
 );
 
-Create table sports_booking
+create table sports_booking
 (
-	booking_id int not null,
+	booking_id varchar(50) not null,
 	status varchar(25),
 	booking_date date,
 	sport_id varchar(50),
 	user_id varchar(50),
-	primary key(sport_id,user_id),
-	-- primary key(booking_id,sport_id,user_id),
+	ts_id varchar(50),
+	primary key(booking_id),
 	foreign key (user_id) references user (user_id) on update cascade on delete cascade,
-	foreign key (sport_id) references sports(sport_id) on delete cascade on update cascade
+	foreign key (sport_id) references sports(sport_id) on delete cascade on update cascade,
+	foreign key (ts_id) references time_slot (ts_id) on delete cascade on update cascade
 );
 
-
-
-Create table event
+create table event
 (
 	event_id varchar(50) not null,
-	event_name varchar(25) not null,
-	e_description varchar(50),
+	event_name varchar(50) not null,
+	e_description varchar(100),
 	start_date date,
 	end_date date,
 	status varchar(25),
-	venue_id int,
+	venue_id varchar(50),
 	capacity int,
 	no_of_participants int,
-	-- user_id varchar(50),
+	organized_by varchar(50) not null,
 	primary key (event_id),
-	-- foreign key (user_id) references [user] (user_id) on update cascade on delete cascade,
-	foreign key(venue_id) references venue(venue_id) on update cascade on delete cascade
+	foreign key(venue_id) references venue(venue_id) on update cascade on delete cascade,
+	foreign key (organized_by) references user (user_id) on update cascade on delete cascade
 );
 
-Create table event_booking
+create table event_booking
 (
-	booking_id int not null,
+	booking_id varchar(50) unique not null,
 	user_id varchar(50),
 	event_id varchar(50),
-	booking_sate date,
+	booking_date date,
 	status varchar(25),
 	primary key (booking_id),
-	-- primary key(event_id,user_id),
 	foreign key (user_id) references user (user_id) on update cascade on delete cascade,
 	foreign key (event_id) references event (event_id) on update cascade on delete cascade
 );
 
-Create table event_type
-(
-	type_id int not null,
-	event_category varchar(25),
-	primary key (type_id)
-);
 
-
-Create table dining (
-	dining_id varchar(50) not null,
+create table dining (
+	dining_id varchar(50) unique not null,
 	type varchar(25),
 	capacity int,
 	booking_time time,
 	primary key (dining_id)
 );
 
-Create table reservation (
+create table reservation (
 	reservation_id varchar(50) unique not null, 
 	reservation_date date,
 	status varchar(25),
 	no_of_ppl int,
 	user_id varchar(50),
 	dining_id varchar(50),
-	-- primary key(user_id,dining_id),
+	primary key(reservation_id),
 	foreign key (user_id) references member(user_id) on update cascade on delete cascade,
 	foreign key (dining_id) references dining(dining_id) on update cascade on delete cascade
 );
 
 Create table party (
-	party_id varchar(50) not null, 
-	user_id varchar(50) ,
+	party_id varchar(50) unique not null, 
+	hosted_by varchar(50) ,
 	p_name varchar(50),
-	venue_id int,
+	hosted_at varchar(50),
 	start_date date,
 	end_date date,
 	no_of_attendees int,
 	status varchar(25),
 	primary key (party_id),
-	foreign key (user_id) references member(user_id) on update cascade on delete cascade,
-	foreign key (venue_id) references venue(venue_id) on update cascade on delete cascade
+	foreign key (hosted_by) references member(user_id) on update cascade on delete cascade,
+	foreign key (hosted_at) references venue(venue_id) on update cascade on delete cascade
 );
