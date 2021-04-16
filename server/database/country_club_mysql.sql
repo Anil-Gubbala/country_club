8dc21157365f3eb4c325ecda60abe0bd2858fa69
Esha Sah <65586427+eshasah@users.noreply.github.com>
2021-04-16 16:25:16 -0700

    Data type for ids set to varchar(50)

--- server/database/country_club_mysql.sql
+++ server/database/country_club_mysql.sql
@@ -3,7 +3,7 @@ create database countryclub;
 use countryclub;
 
 create table user (
-  user_id varchar(50) NOT NULL,
+  user_id varchar(10) NOT NULL,
   f_name varchar(100) NOT NULL,
   l_name varchar(100) NOT NULL,
   email_id varchar(120) NOT NULL,
@@ -19,7 +19,7 @@ create table user (
 
 create table member
 (
-	 user_id varchar(50) unique not null,
+	 user_id varchar(10) unique not null,
 	 membership_type varchar(50) not null,
 	 start_date date not null,
 	 end_date date not null,
@@ -29,7 +29,7 @@ create table member
 create table dependent
 (
 	name varchar(100) not null,
-	user_id varchar(50) not null,
+	user_id varchar(10) not null,
 	relationship varchar(50),
 	primary key (user_id, name),
 	foreign key (user_id) references user (user_id) on update cascade on delete cascade
@@ -37,7 +37,7 @@ create table dependent
 
 create table venue
 (
-	venue_id varchar(50) not null,
+	venue_id varchar(10) not null,
 	venue_name varchar(50),
 	venue_type varchar(25),
 	primary key (venue_id)    
@@ -45,16 +45,16 @@ create table venue
 
 create table sports
 (
-	sport_id varchar(50) not null,
+	sport_id varchar(10) not null,
 	s_name varchar(50),
-	venue_id varchar(50),
+	venue_id varchar(10),
 	primary key (sport_id),
 	foreign key(venue_id) references venue(venue_id) on update cascade on delete cascade
 );
 
 create table time_slot
 (
-	ts_id varchar(50) not null,
+	ts_id varchar(10) not null,
 	slot_start_time time,
 	slot_end_time time,
 	primary key(ts_id)
@@ -62,12 +62,12 @@ create table time_slot
 
 create table sports_booking
 (
-	booking_id varchar(50) not null,
+	booking_id varchar(10) not null,
 	status varchar(25),
 	booking_date date,
-	sport_id varchar(50),
-	user_id varchar(50),
-	ts_id varchar(50),
+	sport_id varchar(10),
+	user_id varchar(10),
+	ts_id varchar(10),
 	primary key(booking_id),
 	foreign key (user_id) references user (user_id) on update cascade on delete cascade,
 	foreign key (sport_id) references sports(sport_id) on delete cascade on update cascade,
@@ -76,13 +76,13 @@ create table sports_booking
 
 create table event
 (
-	event_id varchar(50) not null,
+	event_id varchar(10) not null,
 	event_name varchar(50) not null,
 	e_description varchar(100),
 	start_date date,
 	end_date date,
 	status varchar(25),
-	venue_id varchar(50),
+	venue_id varchar(10),
 	capacity int,
 	no_of_participants int,
 	organized_by varchar(50) not null,
@@ -93,9 +93,9 @@ create table event
 
 create table event_booking
 (
-	booking_id varchar(50) unique not null,
-	user_id varchar(50),
-	event_id varchar(50),
+	booking_id varchar(10) unique not null,
+	user_id varchar(10),
+	event_id varchar(10),
 	booking_date date,
 	status varchar(25),
 	primary key (booking_id),
@@ -105,7 +105,7 @@ create table event_booking
 
 
 create table dining (
-	dining_id varchar(50) unique not null,
+	dining_id varchar(10) unique not null,
 	type varchar(25),
 	capacity int,
 	booking_time time,
@@ -113,19 +113,19 @@ create table dining (
 );
 
 create table reservation (
-	reservation_id varchar(50) unique not null, 
+	reservation_id varchar(10) unique not null, 
 	reservation_date date,
 	status varchar(25),
 	no_of_ppl int,
-	user_id varchar(50),
-	dining_id varchar(50),
+	user_id varchar(10),
+	dining_id varchar(10),
 	primary key(reservation_id),
 	foreign key (user_id) references member(user_id) on update cascade on delete cascade,
 	foreign key (dining_id) references dining(dining_id) on update cascade on delete cascade
 );
 
 Create table party (
-	party_id varchar(50) unique not null, 
+	party_id varchar(10) unique not null, 
 	hosted_by varchar(50) ,
 	p_name varchar(50),
 	hosted_at varchar(50),
