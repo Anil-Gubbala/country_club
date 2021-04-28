89c3e9b81413347a721064a2bde6bbd29091ad82
Esha Sah <65586427+eshasah@users.noreply.github.com>
2021-04-28 14:24:20 -0700

    Debugging approve user

--- server/database/SQL/Admin/adminSql.js
+++ server/database/SQL/Admin/adminSql.js
@@ -2,7 +2,7 @@ const SQL_ADMIN = {
   GET_PENDING_USER_LIST: "select u.user_id, u.f_name, u.l_name, u.street, u.zip_code, u.city, u.status, m.membership_type, \
                           DATE_FORMAT(m.start_date,'%Y-%m-%d') as start_date, DATE_FORMAT(m.end_date,'%Y-%m-%d') as end_date from user u\
                           inner join member m on u.user_id = m.user_id\
-                          where status = 'pending';",
+                          where status = 'Pending';",
 
   GET_USER_BY_ID: "select u.user_id, u.f_name, u.l_name, u.street, u.zip_code, u.city, u.status, m.membership_type, \
                   DATE_FORMAT(m.start_date,'%Y-%m-%d') as start_date, DATE_FORMAT(m.end_date,'%Y-%m-%d') as end_date from user u\
@@ -12,15 +12,15 @@ const SQL_ADMIN = {
   GET_USER_LIST: "select u.user_id, u.f_name, u.l_name, u.street, u.zip_code, u.city, u.status, m.membership_type, \
                   DATE_FORMAT(m.start_date,'%Y-%m-%d') as start_date, DATE_FORMAT(m.end_date,'%Y-%m-%d') as end_date from user u \
                   inner join member m on u.user_id = m.user_id \
-                  where status not like 'pending';",
+                  where status not like 'Pending';",
 
-  APPROVE_USER: "update user set status = 'confirmed' where user_id = ? and status = 'pending';",
+  APPROVE_USER: "update user set status = 'Active' where user_id = ? and status = 'Pending';",
 
-  DELETE_USER: "CALL deleteUser(?)",
+  DELETE_USER: "update user set status = 'Expired' where user_id = ?",
 
   UPDATE_USER_INFO: "CALL updateUserInfo()",
 
-  CREATE_ADMIN: "INSERT INTO countryclub.user(f_name, l_name, email_id, street, city, zip_code, password, auth_id, status) VALUES (?,?,?,?,?,?,?, 1, 'confirmed');"
+  CREATE_ADMIN: "INSERT INTO countryclub.user(f_name, l_name, email_id, street, city, zip_code, password, auth_id, status) VALUES (?,?,?,?,?,?,?, 1, 'Active');"
 
   };
 
