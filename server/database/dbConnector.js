e26c318740a5aedc0cad30ea26d83130193eca91
chitty <lakshmi.gundlapalli@sjsu.edu>
2021-04-29 23:57:12 +0530

    redo db credentials

--- server/database/dbConnector.js
+++ server/database/dbConnector.js
@@ -3,9 +3,9 @@ const mysql = require("mysql");
 const db = mysql.createConnection({
     user: "root",
     host: "localhost",
-    password: "root",
+    password: "test@123",
     database: "countryclub",
-    //port: "3306",
+    port: "3306",
     multipleStatements: true
 });
 
