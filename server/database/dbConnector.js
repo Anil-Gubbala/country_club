57326bfb0c6e62edfebe0788930ddc60b709221c
ISHA RAO <raoisha2193@gmail.com>
2021-04-29 18:15:13 -0700

    reset dbconnector

--- server/database/dbConnector.js
+++ server/database/dbConnector.js
@@ -1,28 +1,16 @@
+
 const mysql = require("mysql");
 
 const db = mysql.createConnection({
     user: "root",
     host: "localhost",
-    password: "Force@22",
+    password: "test@123",
     database: "countryclub",
-    port: "3306"
-  });
-
-  db.connect(function(err) {
-    if (err) {
-      return console.error('error: ' + err.message);
-    }
-
-    console.log('Connected to the MySQL server.');
-  });
-
-  let sql = "SELECT * FROM countryclub.event where event_id='E001'";
-db.query(sql, (error, results, fields) => {
-  if (error) {
-    return console.error(error.message);
-  }
-  console.log(results);
+    port: "3306",
+    multipleStatements: true
 });
+
+//db.commit
 
 
 module.exports = db;
