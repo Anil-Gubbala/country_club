9d90a5ddf711bc450f6a73cc42d07c42cb49ffd1
ISHA RAO <raoisha2193@gmail.com>
2021-05-02 20:05:49 -0700

    DB connector

--- server/database/dbConnector.js
+++ server/database/dbConnector.js
@@ -3,26 +3,11 @@ const mysql = require("mysql");
 const db = mysql.createConnection({
     user: "root",
     host: "localhost",
-    password: "Force@22",
+    password: "test@123",
     database: "countryclub",
-    port: "3306"
+    port: "3306",
+    multipleStatements: true
   });
-
-  db.connect(function(err) {
-    if (err) {
-      return console.error('error: ' + err.message);
-    }
-
-    console.log('Connected to the MySQL server.');
-  });
-
-  let sql = "SELECT * FROM countryclub.event where event_id='E001'";
-db.query(sql, (error, results, fields) => {
-  if (error) {
-    return console.error(error.message);
-  }
-  console.log(results);
-});
 
 
 module.exports = db;
