6c887a84f24ced20798160665be5b1c14c48a96a
ISHA RAO <raoisha2193@gmail.com>
2021-05-02 19:31:07 -0700

    Email functionality added

--- server/database/SQL/Admin/venueSql.js
+++ server/database/SQL/Admin/venueSql.js
@@ -4,7 +4,8 @@ const SQL_VENUE = {
     READ_VENUE: "SELECT * from countryclub.venue where venue_id=?" ,
     UPDATE_VENUE: "UPDATE countryclub.VENUE SET venue_name=?,venue_type=? where venue_id=?",
     DELETE_VENUE: "DELETE from countryclub.VENUE where venue_id=?",
-    GET_VENUE_TYPE: "SELECT venue_type FROM venue_type"
+    GET_VENUE_TYPE: "SELECT venue_type FROM venue_type",
+    GET_VENUE_ID : "SELECT event_id from event where venue_id=?"
   };
   
   module.exports = SQL_VENUE;
